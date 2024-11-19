3be8cffbea46ccd922d1d1ee0d9a0610ffa71d45
Swapnali <swapnalipatil2620@gmail.com>
2024-11-19 13:57:03 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,223 @@
+Instance variables/data members: 
+cno – to store the customer number
+phno – to store the phone number of the customer
+name – to store the name of the customer
+tickets– Use generic Array
+Member methods:
+void addTicket( ) – to add Ticket for this particular instance of customer
+Define a class called Ticket with the following description: 
+
+Instance variables/data members: 
+cost - to Store the Price of ticket.
+ 
+Member methods:
+void computeOverallPrice( ) – which would compute the overall price based on below scheme.
+ 
+Discount and scheme are as follows:
+Above Rs. 70000 – 18% discount
+Rs. 55001 to Rs. 70000 – 16% discount
+Rs. 35001 to Rs. 55000 – 12% discount
+Rs. 25001 to Rs. 350000 – 10% discount
+Less than Rs. 25001 – 2%
+Define a TravelCalc class ,Use generic Array for the implementation of the Customer class.
+
+Default Page Navbar
+
+InnerText	Path	ControlId
+Home	/	home(click event)
+Customer Form	/customerForm.html	customerForm(click event)
+Ticket Form	/ticketForm.html	ticketForm(click event)
+ 
+
+On click event  should be redirected to respected URL mentioned in the path.
+
+Home Page
+
+Home Page should consist of table which would consists of following information
+
+Customer Table and Customer Ticket History which will be nested table. 
+
+Note: Nested Table Id should be as follow for example: customer(cno: 001, ...)
+
+<table id="nestedTable-001"></table>
+
+Table should consist of following field as shown in below image
+
+Image
+
+ 
+
+ 
+
+Customer Form
+
+Customer Form should consist of following field
+
+Note: On Successful validition the onClick of the Submit Button user should be redirect to Home Page
+ 
+
+ 
+
+Field	Type	ControlId	ControlId For Error	Validation
+Customer Number	string	customerNo	customerNumberError	
+ 
+
+Validation Type	Validaton error
+Required	Customer Number Required
+Unique	Customer Number Already Present
+ 
+
+Customer Phone Number	number	customerPhNo	customerPhNoError	
+ 
+
+Validation Type	Validaton error
+Required	Phone Number Required
+Length	Phone Number length must be 10
+ 
+
+Customer Name	string	customerName	customerNameError	
+ 
+
+Validation Type	Validaton error
+Required	Customer Name Required
+Length	Customer Name length should at least 3 character and at most 10 character
+ 
+
+Add Customer	submit	addCustomerFormBtn	 	 
+ 
+
+Ticket Form
+
+Ticket Form should consist of following field
+
+Note: Customer Number should be binded with the Customer details
+
+  i.e for Customer (cno: 001, name: Test) <option value="001">Test</option>
+
+Note: On Successful validition the onClick of the Submit Button user should be redirect to Home Page
+
+ 
+
+Field	Type	ControlId	ControlId For Error	Validation
+Select customer Number	DropDown	customerNumber	customerNumberDropDownError	
+ 
+
+Validation Type	Validaton error
+Required	Select customer Number
+ 
+
+Ticket Cost	Number	ticketCost	ticketCostError	
+ 
+
+Validation Type	Validaton error
+Required	Ticket Cost Required
+Negative	Ticket Cost must be postive
+ 
+
+Add Ticket	submit	ticketFormSubmitBtn	 	 
+ 
+
+ 
+
+Application should consist of three pages.
+
+ 
+
+ 
+
+InnerText	Path	ControlId
+Home	/	home
+Customer Form	/customerForm	customerForm
+Ticket Form	/ticketForm	ticketForm
+ 
+
+Home Page
+
+Home Page should consist of table which would consists of following information
+
+Customer Table and Customer Ticket History which will be nested table. 
+
+Note: Nested Table Id should be as follow for example: customer(cno: 001, ...)
+
+<table id="nestedTable-001"></table>
+
+Table should consist of following field as shown in below image
+
+ 
+
+ 
+
+Customer Form
+
+Customer Form should consist of following field
+
+Note: On Successful validition the onClick of the Submit Button user should be redirect to Home Page
+ 
+
+ 
+
+Field	Type	ControlId	ControlId For Error	Validation
+Customer Number	string	customerNo	customerNumberError	
+ 
+
+Validation Type	Validaton error
+Required	Customer Number Required
+Unique	Customer Number Already Present
+ 
+
+Customer Phone Number	number	customerPhNo	customerPhNoError	
+ 
+
+Validation Type	Validaton error
+Required	Phone Number Required
+Length	Phone Number length must be 10
+ 
+
+Customer Name	string	customerName	customerNameError	
+ 
+
+Validation Type	Validaton error
+Required	Customer Name Required
+Length	Customer Name length should at least 3 character and at most 10 character
+ 
+
+Add Customer	submit	addCustomerFormBtn	 	 
+ 
+
+Ticket Form
+
+Ticket Form should consist of following field
+
+Note: Customer Number should be binded with the Customer details
+
+  i.e for Customer (cno: 001, name: Test) <option value="001">Test</option>
+
+Note: On Successful validition the onClick of the Submit Button user should be redirect to Home Page
+
+ 
+
+Field	Type	ControlId	ControlId For Error	Validation
+Select customer Number	DropDown	customerNumber	customerNumberDropDownError	
+ 
+
+Validation Type	Validaton error
+Required	Select customer Number
+ 
+
+Ticket Cost	Number	ticketCost	ticketCostError	
+ 
+
+Validation Type	Validaton error
+Required	Ticket Cost Required
+Negative	Ticket Cost must be postive
+ 
+
+Add Ticket	submit	ticketFormSubmitBtn	 
+
+
+
+
+
 interface customer{
   cno: string;
   phno: number;
